7ee81590456aec4d4e16412e53581b290975a28d
mriduladas <mridula199@gmail.com>
2022-05-15 01:45:26 +0530

    Testing click event which changes state

--- src/Greetings.js
+++ src/Greetings.js
@@ -1,10 +1,20 @@
 import { Fragment } from "react"
+import { useState } from "react";
 
 const Greetings = () => {
+
+    const [changedText, setChangedText] = useState(false);
+
+    const onClickHandler= () => {
+        setChangedText(true);
+    }
     return (
         <div>
             <h2>Hello World</h2>
+            {!changedText && <p>This is default text</p> }
+            {changedText && <p>Text is changed !!</p>}
             <p>This is a demo testing project</p>
+            <button onClick={onClickHandler}>Change Text</button>
         </div>
     )
 }
--- src/Greetings.test.js
+++ src/Greetings.test.js
@@ -1,9 +1,10 @@
 import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Greetings from './Greetings'
 
-describe('Greetings component test', () => {
-    test('hello world test in Greetings', ()=>{
+describe('Greetings component', () => {
+    test('has hello world text', ()=>{
         //Arrange
         render(<Greetings />)
         //Act
@@ -12,6 +13,40 @@ describe('Greetings component test', () => {
         const helloWorldElement = screen.getByText("Hello World", {exact: true})
         expect(helloWorldElement).toBeInTheDocument();
     
+    })
+
+    test('renders This is default text when button is not clicked', ()=>{
+        //Arrange
+        render(<Greetings />)
+        //Act
+            //..Nothing for this test
+        //Assert
+        const helloWorldElement = screen.getByText("This is default text", {exact: true})
+        expect(helloWorldElement).toBeInTheDocument();
+    
+    })
+
+    test('renders Text is changed when the button is clicked', ()=>{
+        //Arrange
+        render(<Greetings />)
+        //ACT
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+        //Assert
+        //Here test is Text is changed!! in componet so gave false
+        const text = screen.getByText('Text is changed', {exact:false}); 
+        expect(text).toBeInTheDocument();
+    })
+
+    test('does not render This is default text when button is clicked', ()=> {
+        //Arrange
+        render(<Greetings />);
+        //ACT
+        const buttonElement = screen.getByText('Change Text');
+        userEvent.click(buttonElement);
+        //Asset
+        const text = screen.queryByText('This is default text', {exact: false});
+        expect(text).toBeNull();
     })
 })
 
